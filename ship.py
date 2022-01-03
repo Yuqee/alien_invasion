e3c9d1799f783d9c3cd951f9ed8c304644046ac5
Yuqi Liu <billionanna@outlook.com>
2022-01-03 21:44:52 +0800

    Stage 3. a, ship is able to move right and left. b, ship does go beyond screen. c, press Q to exit game. d, Fullscreen mode.

--- ship.py
+++ ship.py
@@ -7,12 +7,38 @@ class Ship:
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
         # Load the ship image and get its rect.
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
+
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Store a decimal value for the ship's horizontal position.
+        # Because ship_speed in Settings is 1.5 which will make the ship's 
+        # horizontal position become a decimal value. Moreover, since rectangle
+        # object only keep the integer position of that value. Hence, for 
+        # accurancy, we need self.x to keep track of the accurate position.
+        self.x = float(self.rect.x)
+
+        # Movement flag
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Update the ship's position based on the movement flag."""
+        # Update the ship's x value, not the rect.
+        # The 2nd condition is limiting the range of the ship, so that it does not
+        # go out of the screen.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > self.screen_rect.left:
+            self.x -= self.settings.ship_speed
+
+        # Update rect object from self.x
+        self.rect.x = self.x
 
     def blitme(self):
         """Draw the ship at its current location."""
